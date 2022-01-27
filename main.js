a3db297d7d0e8e958fa89c20f504739f7caceb69
Alex <alex.chart@turningpointhr.com>
2022-01-27 01:56:34 +0000

    commit tree 7b7dd939fd14c0e5bc2c8b6ca1d7a2dec4520c4f

--- main.js
+++ main.js
@@ -1,11 +1,68 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const errorModal = document.querySelector("#modal")
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM CONTENT HAS LOADED")
+  errorModal.classList.add("hidden")
+  findLikes()
+})
 
+function hideError(){
+  errorModal.classList.add("hidden")
+}
 
+function findLikes(){
+const likeArr = document.querySelectorAll(".like-glyph")
+
+likeArr.forEach((singularLike) => {
+  singularLike.addEventListener("click", (event) => {
+    mimicServerCall() 
+    .then(() => {
+      if (event.target.innerText === EMPTY_HEART){
+      event.target.classList.add('activated-heart')
+      event.target.innerText = FULL_HEART
+      }
+      else if (event.target.innerText === FULL_HEART){
+        event.target.classList.remove('activated-heart')
+        event.target.innerText = EMPTY_HEART
+      }
+    })
+
+    .catch(error => {
+      setTimeout(() => {
+        hideError()
+      },3000)
+  })
+})
+})
+}
+
+// document.addEventListener('click', (event) => {
+//   if(event.target.classList.value === 'like-glyph'){
+
+//     mimicServerCall() 
+//     .then(() => {
+//       if (hearts.target.innerText === EMPTY_HEART){
+//       hearts.target.classList.add('activated-heart')
+//       hearts.target.innerText = FULL_HEART
+//       }
+//       else if (hearts.target.innerText === FULL_HEART){
+//         hearts.target.classList.remove('activated-heart')
+//         hearts.target.innerText = EMPTY_HEART
+//       }
+//     })
+
+//     .catch(error => {
+//       setTimeout(() => {
+//         hideError()
+//       },3000)
+//   })
+// }
+// })
+// }
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
